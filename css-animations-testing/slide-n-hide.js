e731390b11ff2851e7ca509244fa8343cf3daac8
Mehul Gangavelli <mehul.gang@gmail.com>
2015-12-07 19:39:49 -0500

    Messing around with JQuery getting various parameters on the sliderContainer.

--- css-animations-testing/slide-n-hide.js
+++ css-animations-testing/slide-n-hide.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
+var elementOffset, elementWidth, elementHeight;
 
+$(document).ready(function() {
     $("#hiderButton1").click(function(){
+        var string = "";
+        elementOffset = $("#sliderContainer1").offset();
+        elementWidth = $("#sliderContainer1").width();
+        elementHeight = $("#sliderContainer1").height();
+        string = "height "+elementHeight+" width "+elementWidth+
+        " offset "+elementOffset.top+","+elementOffset.left + "\n";
+
+        alert(string);
         $("#sliderContainer1").addClass("hideNSlide");
     });
 });
